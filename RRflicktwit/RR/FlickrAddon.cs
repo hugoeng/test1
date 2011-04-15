0fed4c8085034a41d15d708c1f75f458eb7fbed9
Hugo Engström <hugo.engstrom@gmail.com>
2011-04-15 15:40:00 +0200

    initial

---
+++ RRflicktwit/RR/FlickrAddon.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlickrNet;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Rapid_Reporter
+{
+   public class FlickrAddon
+    {            
+        private Flickr m_flickr;
+        private string m_frob;
+        private Auth m_auth;
+        public static string hashcode;
+
+        private string API_KEY = "d06d4e9bb977706b443f5cb969b29a94";
+        private string SECRET = "a62d4e4bed745bf0";
+
+        public void Login()
+        {
+
+            // Create Flickr instance
+            m_flickr = new Flickr(API_KEY, SECRET);
+
+            m_frob = m_flickr.AuthGetFrob();
+
+            string flickrUrl = m_flickr.AuthCalcUrl(m_frob, AuthLevel.Write);
+
+            // The following line will load the URL in the users default browser.
+            System.Diagnostics.Process.Start(flickrUrl);
+           
+            bool bIsAuthorized = false;
+            m_auth = new Auth();
+
+            // do nothing until flickr authorizes.
+            while (!bIsAuthorized)
+            {
+                try
+                {
+                    m_auth = m_flickr.AuthGetToken(m_frob);
+                    m_flickr.AuthToken = m_auth.Token;
+                    bIsAuthorized = true;
+                }
+                catch (FlickrException ex)
+                {
+                	//TODO
+                    string s = ex.Message;
+                }
+            }
+             
+        }
+
+        public static string GetUniqueKey(int length)
+        {
+            string guidResult = string.Empty;
+
+            while (guidResult.Length < length)
+            {
+                // Get the GUID.
+                guidResult += Guid.NewGuid().ToString().GetHashCode().ToString("x");
+            }
+
+            // Make sure length is valid.
+            if (length <= 0 || length > guidResult.Length)
+                throw new ArgumentException("Length must be between 1 and " + guidResult.Length);
+
+            // Return the first length bytes.
+            hashcode = guidResult.Substring(0, length);
+            return hashcode;
+        }
+        public string GetCurrentUser()
+        {
+            return m_auth.User.UserName;
+        }
+
+        public string Upload(string pFile, string pName, string pDesc, string pTags)
+        {
+
+            if (SMWidget.ToggleUploadFlickr == true)
+            {
+                try
+                {
+                    bool b = m_flickr.IsAuthenticated;
+                }
+                catch (FlickrException f)
+                {
+                    System.Windows.MessageBox.Show(f.Message); 
+                }
+
+                if (IsAuthenticated())
+                {
+                    return m_flickr.UploadPicture(pFile, pName, pDesc, pTags,true,true,true);
+
+                }
+                else
+                {
+                    Login();
+                    return m_flickr.UploadPicture(pFile, pName, pDesc, pTags,true,true,true);
+                }
+            }
+            return "";
+        }
+
+        public bool IsAuthenticated()
+        {
+            return m_flickr.IsAuthenticated;
+        }
+
+
+        public string GetUrl(string id)
+        {
+            PhotoInfo info = m_flickr.PhotosGetInfo(id);
+            return info.WebUrl;
+        }
+
+        public void LogOut()
+        {
+            m_flickr.AuthToken = null;
+            System.Diagnostics.Process.Start("http://www.flickr.com/logout.gne?");
+        }
+
+    }
+
+    
+
+
+
+    
+}
+
+
---
+++ RRflicktwit/RR/TwitterAddon.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using Twitterizer.Core;
+using Twitterizer;
+
+namespace Rapid_Reporter
+{
+    public class TwitterAddon
+    {
+        // Post to Twitter enabled (true) or disabled (false)
+        public static bool twitter = true;
+
+        // TwitterLogin variables
+        private static string requestToken;
+
+        // TwitterOAuth variables
+        public static OAuthTokens tokens;
+        public static string twitterPIN;
+        public static string ScreenName;
+        private static decimal UserID;
+
+        // PostOnTwitter variables
+        public static string tempNote;
+        public static string tempTester;
+        public static string tempCharter;
+        public static string[] tempNoteTypes;
+        public static int tempType;
+
+        // Starts your default internet browser and takes you to a twitter page where you can
+        // login with any account and allow Rapid Reporter to use the account for Twitter posting.
+        // You then get a PIN code to enter in Rapid Reporter.
+        public static void TwitterLogin()
+        {
+            requestToken = OAuthUtility.GetRequestToken("WxOSthlIyUDQzWiGzY7F5Q", "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM", "oob").Token;
+
+            Process browserProcess = new Process();
+            browserProcess.StartInfo.FileName = OAuthUtility.BuildAuthorizationUri(requestToken).AbsoluteUri;
+            if (browserProcess.Start())
+            {
+                browserProcess.WaitForExit();
+            }
+            else
+            {
+                //Could not start your browser
+            }
+        }
+
+        // Verifies the entered PIN code and creates token, ScreenName and so on.
+        public static void TwitterOAuth()
+        {
+            OAuthTokenResponse accessToken = OAuthUtility.GetAccessToken("WxOSthlIyUDQzWiGzY7F5Q", "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM", requestToken, TwitterAddon.twitterPIN);
+
+            tokens = new OAuthTokens();
+            tokens.AccessToken = accessToken.Token;
+            tokens.AccessTokenSecret = accessToken.TokenSecret;
+            tokens.ConsumerKey = "WxOSthlIyUDQzWiGzY7F5Q";
+            tokens.ConsumerSecret = "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM";
+            ScreenName = accessToken.ScreenName;
+            UserID = accessToken.UserId;
+        }
+
+        // Posts message on Twitter and truncates if too long.
+        public static void PostOnTwitter()
+        {
+            // What's going to show on Twitter
+            string twitterPost = "[Reporter: " + tempTester + ", Charter: " + tempCharter + "] " + tempNote + " #" + tempNoteTypes[tempType] + " #" + FlickrAddon.hashcode;
+
+            string twitterNote;
+            int twitterNoteLength;
+            int twitterNumberOfCharsToRemove;
+
+            if (twitterPost.Length > 140)
+            {
+                // How many characters to remove.
+                twitterNumberOfCharsToRemove = twitterPost.Length - 140;
+                // On which character to start removing.
+                twitterNoteLength = tempNote.Length - twitterNumberOfCharsToRemove;
+                // Removes the characters
+                twitterNote = tempNote.Substring(0, twitterNoteLength);
+                // What's going to show on Twitter if 'note' is too long and truncated.
+                twitterPost = "[Reporter: " + tempTester + ", Charter: " + tempCharter + "] " + twitterNote + " #" + tempNoteTypes[tempType] + " #" + FlickrAddon.hashcode;
+            }
+                 
+            // Posts on Twitter.
+            TwitterResponse<TwitterStatus> tweetResponse = TwitterStatus.Update(TwitterAddon.tokens, twitterPost);
+            
+        }
+    }
+}
